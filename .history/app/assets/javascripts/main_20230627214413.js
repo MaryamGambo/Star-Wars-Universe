39dd0adef449605b1a4f061b9f7c1423877107d3
Maryam Ayemlo Gambo <82781910+MaryamGambo@users.noreply.github.com>
2023-06-27 22:28:19 -0500

    Added bulma styling to the pages

---
+++ .history/app/assets/javascripts/main_20230627214413.js
@@ -0,0 +1,31 @@
+// main.js
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Get all "navbar-burger" elements
+  const navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll('.navbar-burger'),
+    0
+  );
+
+  // Check if there are any navbar burgers
+  if (navbarBurgers.length > 0) {
+    // Add a click event listener to each navbar burger
+    navbarBurgers.forEach((navbarBurger) => {
+      navbarBurger.addEventListener('click', () => {
+        // Get the target menu from the data-target attribute
+        const target = navbarBurger.dataset.target;
+        const targetMenu = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the navbar-burger and the target menu
+        navbarBurger.classList.toggle('is-active');
+        targetMenu.classList.toggle('is-active');
+
+        // Toggle the background color on link lists when the menu is active
+        const linkLists = document.querySelectorAll('.navbar-menu .navbar-start');
+        linkLists.forEach((linkList) => {
+          linkList.classList.toggle('has-background-dark');
+        });
+      });
+    });
+  }
+});
---
+++ .history/app/models/person_20230627181909.rb
@@ -0,0 +1,12 @@
+class Person < ApplicationRecord
+  belongs_to :planet, optional:true
+  belongs_to :species, optional:true
+  has_and_belongs_to_many :starships
+  has_and_belongs_to_many :films
+  has_and_belongs_to_many :vehicles
+
+  # validations
+  validates :name, presence:true
+  validates :url, presence:true
+  paginates_per 15
+end
